6f8a67824d7ae5946e591d583ea377c551563208
Kiran Rajput <rajputkiran2805@gmail.com>
2024-09-19 15:12:23 +0530

    created JPA repository

--- src/main/java/com/guardians/repository/UrlMappingRepository.java
+++ src/main/java/com/guardians/repository/UrlMappingRepository.java
@@ -9,5 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
-    Optional<UrlMappingDto> findByShortUrl(String shortUrl);
+    Optional<UrlMapping> findByShortUrl(String shortUrl);
 }
