1d5f75a18003ada24765ac4ab68eac3b73857c49
Kiran Rajput <rajputkiran2805@gmail.com>
2024-09-26 20:14:43 +0530

    updated url mapping

--- src/main/java/com/guardians/model/UrlMapping.java
+++ src/main/java/com/guardians/model/UrlMapping.java
@@ -1,8 +1,10 @@
 package com.guardians.model;
 
 
+import com.guardians.auth.entities.User;
 import com.guardians.dto.UrlMappingDto;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,6 +17,8 @@ public class UrlMapping {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "The original URL field can't be blank")
     private String originalUrl;
     private String shortUrl;
     private LocalDateTime createdAt;
@@ -22,8 +26,10 @@ public class UrlMapping {
     private int usageLimit;
     private int usageCount;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
 
-
+    private User user;
     // Getters and Setters
 }
 
