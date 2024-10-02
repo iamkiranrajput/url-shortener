311d03410f9f0440b87eb3e4151437f5754fbb7c
Kiran Rajput <rajputkiran2805@gmail.com>
2024-10-02 22:10:08 +0530

    Rating model

---
+++ src/main/java/com/guardians/model/Rating.java
@@ -0,0 +1,26 @@
+package com.guardians.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long urlId;
+    private Long userId;
+    private Long rating;
+    private LocalDateTime createdAt;
+
+}
