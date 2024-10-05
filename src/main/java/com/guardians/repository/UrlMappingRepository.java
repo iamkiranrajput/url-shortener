e897c18b64efd352811ec3f8eedaae37de1467b4
Kiran Rajput <rajputkiran2805@gmail.com>
2024-10-05 21:34:06 +0530

    updated urlMapping repository

--- src/main/java/com/guardians/repository/UrlMappingRepository.java
+++ src/main/java/com/guardians/repository/UrlMappingRepository.java
@@ -2,12 +2,21 @@ package com.guardians.repository;
 
 
 
+import com.guardians.auth.entities.User;
 import com.guardians.dto.UrlMappingDto;
 import com.guardians.model.UrlMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
+
+@Repository
 public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
     Optional<UrlMapping> findByShortUrl(String shortUrl);
+    Optional<UrlMapping> findById(int urlId);
+    List<UrlMapping> findByUser(User user);
+
+
 }
